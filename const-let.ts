6e240f6fe2f8dbf27ca5546240234018d6ff9e5e
Nathan Ho <nathanho@Nathans-MacBook-Pro.local>
2020-07-10 20:41:09 -0400

    const vs let explained

--- const-let.ts
+++ const-let.ts
@@ -2,3 +2,18 @@ const age: number = 42;
 let foo: string = 'bar';
 
 console.log(foo, age);
+
+// age = 42 not allowed bc of const keyword
+foo = 'quack';
+
+const person = {
+    name: 'nathan',
+}
+
+// person = {
+//     name: 'tony'
+// }
+
+person.name = 'tony';
+console.log('person (ln#: 18)', person);
+console.log('person name (ln#: 19)', person.name);
